f9c7ef6caeac2364d6706f742903265c00b3ffde
AsadbekNurmamtov2002 <nurmamatovasadbek85@gmail.com>
2024-05-22 01:37:52 +0500

    mk

--- README.md
+++
@@ -1,1 +0,0 @@
-# AUSCoders Jamosi
--- script.js
+++
@@ -1,7 +0,0 @@
-let navLinks = document.getElementById("navLinks");
-function showMenu(){
-    navLinks.style.top = '0';
-}
-function hideMenu(){
-    navLinks.style.top = '-800px'
-}
